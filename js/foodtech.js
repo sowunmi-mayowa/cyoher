da18335e71a031fd6fcb8359c5b2e7997628e485
sowunmi mayowa <smayowa176@gmail.com>
2022-11-18 08:56:27 +0100

    update

---
+++ js/foodtech.js
@@ -0,0 +1,49 @@
+const aiyeleye = document.querySelector('.aiyeleye');
+const ajayi = document.querySelector('.ajayi');
+const akoja = document.querySelector('.akoja');
+const noah = document.querySelector('.noah');
+const makanju = document.querySelector('.makanju');
+const adebowale = document.querySelector('.adebowale');
+
+aiyeleye.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: "AIYELEYE FRANCIS BANKOLE,PHD",
+        text: 'Aiyeleye, F. B. has obtained a Bachelor of Science degree in Biochemistry from the University of Lagos, and completed his MSc and PhD at the University of Ibadan, Ibadan and Federal University of Agriculture, Abeokuta, Nigeria respectively. He has over 35 years of teaching experience in Food Processing and Preservation. He has authored and co-authored books and publications in peer-reviewed journals. He has served as Head of Department and Dean of the School, at different times in his career. He also provides consultancy services to various food processing industries'
+    });
+})
+ajayi.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: "AJAYI, ADEBOLA",
+        text: 'Mr. Adebola Ajayi, Food Technologist with focus on Food Eniginering. He had his undergraduate degree in Food Technology from the University of Ibadan, Ibadan in 1984 and Masters in Food Technology in the same University in 1990. Over the years he has been engaged in meaningful research and teaching. He has conducted researches in Food Engineering with special interest in Food Dehydration, Rheology and Fluid Mechanics. He has mentored a good number of students both at the National and Higher National Diploma levels. He is a Senior lecturer in the Department of Food Technology, Federal Polytechnic, Ilaro in Ogun State.'
+    });
+})
+akoja.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: "AKOJA, SUNDAY SAMUEL.",
+        text: 'Sunday is a versatile lecturer, of the Federal Polytechnic Ilaro, in the department of Food technology in the school of Pure and Applied sciences. He lectures students on courses like Food Chemistry, Water and Waste Management and Entrepreneurship. Due to his passion for a sustainable food security system and quest to make progress in reducing protein malnutrition, he majors in value-addition to snacks through under-exploited leguminous crop seeds. He is an entrepreneur. Sunday is a thorough scientist with good communication skills who passes his messages to his listeners in a more simple and comprehensible manner. His experience spans over a wide range of Food Science and Technology Field with up to date knowledge of events. He is a member of Nigerian institute of Science Laboratory TechnologyHe is a regular attendee of local and international conferences and workshops. He is a motivator. He is married to Mrs. Noimot .A. Akoja with four children.'
+    });
+})
+noah.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: "NOAH, ABIMBOLA ADUKE",
+        text: 'Noah Abimbola Aduke obtained a B.Sc in Microbiology from Ogun State University and M.Sc in Microbiology from the University of Lagos. She has lectured both in College of Education and Polytechnic for a number of years. She has not only been involved in classroom teaching butalso had working experience in the Food and Beverage Industry.During her years of academic career, sheattended and presented technical papers in several International Conferences, Workshops and Seminars. A number of her research have been published in reputable journals. She is a seasoned lecturer and researcher. Her teaching experience spansclose totwo decades. She is currently a Principal Lecturer in the Department of Food Technology, Federal Polytechnic, Ilaro,Ogun State. She is married with children.'
+    });
+})
+makanju.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: "MAKANJUOLA, OLAKUNLE MOSES",
+        text: 'Makanjuola, Olakunle Moses is a product  of two most valuable systems of Education, i.e Polytechnic and University. He has Masters Degree (Masters of Technology) in Food Science and Technolgy from the Federal University of Technology, Akure, Ondo State. He has over 25 years expereience as an academic staff of the Federal Polytechnic, Ilaro, Ogun State. He is the Zonal Coordinator (Ogun zone) of the Presitgious Nigeria Institure of Food Science and Technolgy (NIFST).Presently, He is a Principal Lecturer in the Department of Food Technology Federal Polytechnic, Ilaro, where he teaches course in Processing and Preservation of both Animal and Plant Food Products, Food Quality Control Statistics, Food Quality Control (Animal Products), Canning Techology, Food Legislation, Food Science and Nutriton among Others. He is also a consultant to Lagos State Government on Safety, Health and wellbeing where he had delivered several papers. He has several Publications (Journals) to his credit and has also presented Papers in conferences both locally and internationally. He has co-authored two major text books in Food Science and Techology. He is happyly married and blessed with Children'
+    });
+})
+adebowale.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: "ADEBOWALE OLALEKAN JIMI",
+        text: 'Adebowale, Olalekan holds first and second degrees in Food Science and Technology from the Federal University of Agriculture, Abeokuta. he joined the Federal Polytechnic, Ilaro as Lecturer I in the Department of Food Technology. He was appointed as the Departmental Examination Officer, a post he held for 8 years without any form of misconduct. As a lecturer, he was involved in teaching, supervising students’ projects and research. He has been a participant of the Step-B World Bank Project for the upgrading of facilities in Food Processing and Engineering workshops. He also participated in some TETFUND-sponsored projects in the department. He is a member of the Nigerian Institute of Food Science and Technology (NIFST) and the South African Association of Food Science and Technology (SAAFoST). He has many publications in peer-reviewed journals both in Nigeria and overseas. He is a reviewer of some journals. Presently, he is a Doctoral candidate at the University of Pretoria, South Africa.'
+    });
+})
